d78df67f44abf86b55229f2ec8db9257b1d739b1
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-05-15 18:33:31 +0200

    Revert "No more support for separate env_file param"
    This reverts commit faae2dd3ba5a919aa1d6f0af959326d323af4cec.

--- plugins/modules/ibridges_sync.py
+++ plugins/modules/ibridges_sync.py
@@ -25,9 +25,14 @@ options:
         description: The local path (on the Ansible target) to be copied to or synced from.
         required: true
         type: str
+    env_file:
+        description: The local path (on the Ansible target) to read the iRODS environment file from.
+        required: false
+        type: str
+        default: ~/.irods/irods_environment.json
     env:
         description: A dictionary containing all the information about your iRODS environment.
-        required: true
+        required: false
         type: str
     password:
         description: The password to use to connect to iRODS.
@@ -58,7 +63,7 @@ EXAMPLES = r'''
 - name: Sync an iRODS path to a local path
   uusrc.ibridges.sync:
     mode: down
-    env: /home/user/.irods/irods_environment.json
+    env_file: /home/user/.irods/irods_environment.json
     irods_path: ResearchData/testdata
     local_path: /tmp/test
     password: letmein
@@ -67,9 +72,7 @@ EXAMPLES = r'''
 - name: Sync an iRODS path to a local path
   uusrc.ibridges.sync:
     mode: up
-    env:
-      irods_host: irods.foo.com
-      irods_home: /zone/foo
+    env_file: /home/user/.irods/irods_environment.json
     irods_path: ResearchData/testdata
     local_path: /tmp/test
     password: letmein
@@ -97,8 +100,9 @@ def run_module():
     module_args = dict(
         irods_path=dict(type='str', required=True),
         local_path=dict(type='str', required=True),
-        env=dict(type='str', required=True),
+        env=dict(type='str', required=False),
         mode=dict(type='str', required=False, default='down'),
+        env_file=dict(type='str', required=False, default="~/.irods/irods_environment.json"),
         password=dict(type='str', required=True, no_log=True),
         max_level=dict(type='int', required=False, default=0),
         copy_empty_folders=dict(type='bool', required=False, default=True),
@@ -118,7 +122,12 @@ def run_module():
 
     from ibridges import Session
     from pathlib import Path
-    session = Session(irods_env=module.params['env'], password=module.params['password'])
+    if module.params['env']:
+        session = Session(irods_env=module.params['env'], password=module.params['password'])
+    elif module.params['env_file']:
+        session = Session(irods_env_path=module.params['env_file'], password=module.params['password'])
+    else:
+        module.fail_json(msg='Neither env nor env_file were specified, do not know how to continue.', changed=False)
 
     try:
         from ibridges import sync_data, IrodsPath
