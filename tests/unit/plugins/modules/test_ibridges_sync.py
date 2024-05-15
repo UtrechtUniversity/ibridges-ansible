358ba69d458df789278ac747c249dcb3842b6c9a
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-05-15 14:21:00 +0200

    Linting

--- tests/unit/plugins/modules/test_ibridges_sync.py
+++ tests/unit/plugins/modules/test_ibridges_sync.py
@@ -2,7 +2,7 @@ import json
 import unittest
 import copy
 
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from ansible.module_utils import basic
 from ansible.module_utils.common.text.converters import to_bytes
 from plugins.modules import ibridges_sync
@@ -11,7 +11,8 @@ DEFAULT_ARGS = {
     'irods_path': '/test',
     'local_path': '/tmp/',
     'password': 'test',
-} # This includes only required arguments
+}  # This includes only required arguments
+
 
 class MockedSession():
     def __init__(self, **kwargs):
@@ -20,6 +21,7 @@ class MockedSession():
         self.password = 'password'
         self.irods_session = 'mocked'
 
+
 def set_module_args(args):
     """prepare arguments so that they will be picked up during module creation"""
     if not args:
@@ -27,23 +29,27 @@ def set_module_args(args):
     args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
     basic._ANSIBLE_ARGS = to_bytes(args)
 
+
 def setup_sync(check_mode=False, **kwargs):
     args = copy.deepcopy(DEFAULT_ARGS)
     args['env'] = {
-    'irods_host': 'nowhere'
+        'irods_host': 'nowhere'
     }
     args['_ansible_check_mode'] = check_mode
     args = dict(**args, **kwargs)
     set_module_args(args)
     return args
 
+
 class AnsibleExitJson(Exception):
     """Exception class to be raised by module.exit_json and caught by the test case"""
     pass
 
+
 class AnsibleFailJson(Exception):
     """Exception class to be raised by module.fail_json and caught by the test case"""
     pass
+
 
 def exit_json(*args, **kwargs):
     """function to patch over exit_json; package return data into an exception"""
@@ -80,15 +86,15 @@ class TestiBridgesSync(unittest.TestCase):
     def test_sync_down(self, mocked_session, mocked_sync):
         mocked_session.return_value = MockedSession()
         mocked_sync.return_value = {
-        'changed_files': ['foo'],
-        'changed_folders': ['bar']
+            'changed_files': ['foo'],
+            'changed_folders': ['bar']
         }
 
         args = setup_sync()
 
         with self.assertRaises(AnsibleExitJson) as context:
             ibridges_sync.main()
-        
+
         expectation = {
             'changed': True,
             'msg': '',
@@ -118,15 +124,15 @@ class TestiBridgesSync(unittest.TestCase):
     def test_sync_up(self, mocked_session, mocked_sync):
         mocked_session.return_value = MockedSession()
         mocked_sync.return_value = {
-        'changed_files': [],
-        'changed_folders': []
+            'changed_files': [],
+            'changed_folders': []
         }
 
         args = setup_sync(mode='up')
 
         with self.assertRaises(AnsibleExitJson) as context:
             ibridges_sync.main()
-        
+
         expectation = {
             'changed': False,
             'msg': '',
@@ -143,21 +149,20 @@ class TestiBridgesSync(unittest.TestCase):
         self.assertEqual(call.kwargs['source'], args['local_path'])
         self.assertEqual(call.kwargs['target'].absolute_path(), args['irods_path'])
 
-
     @patch('ibridges.sync_data')
     @patch('ibridges.Session')
     def test_sync_dry_run(self, mocked_session, mocked_sync):
         mocked_session.return_value = MockedSession()
         mocked_sync.return_value = {
-        'changed_files': ['foo'],
-        'changed_folders': ['bar']
+            'changed_files': ['foo'],
+            'changed_folders': ['bar']
         }
 
         args = setup_sync(check_mode=True)
 
         with self.assertRaises(AnsibleExitJson) as context:
             ibridges_sync.main()
-        
+
         expectation = {
             'changed': False,
             'msg': 'Executed iBridges dry run.',
